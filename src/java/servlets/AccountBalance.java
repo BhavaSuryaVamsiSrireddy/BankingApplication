f3784d2ae0fe7e745fb41889b7cb96a5e36abde3
BhavaSuryaVamsiSrireddy <43814473+BhavaSuryaVamsiSrireddy@users.noreply.github.com>
2020-07-29 16:54:23 +0530

    Update AccountBalance.java

--- src/java/servlets/AccountBalance.java
+++ src/java/servlets/AccountBalance.java
@@ -12,6 +12,7 @@ import classes.*;
 public class AccountBalance extends HttpServlet{
     public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException{
         response.setContentType("text/html");
+        //getting the account_balance from database using the static method displayAccountBalance in AccountsOperation class in classes package.
         int rs = AccountOperations.displayAccountBalance();
         request.setAttribute("result", rs);
         request.getRequestDispatcher("DisplayBalance.jsp").forward(request, response);
